928bfc466276b83a1b1539af127ee627ed6d3aea
Luca Corona <lucacorona@email.com>
2026-05-03 10:34:43 +0200

    Add vercel domain to CORS

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://budget-tracker.vercel.app",
+        "https://project-pyim2.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
